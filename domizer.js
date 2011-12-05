c38b54779998294043a6ca3e6947b5b04edc53f1
Adrien Risser <risseraka@gmail.com>
2011-12-05 22:43:24 +0100

    Update a few things, added an example

--- domizer.js
+++ domizer.js
@@ -1,39 +1,40 @@
-var tags = [
-    "a", "abbr", "address", "area", "article", "aside", "audio",
-    "b", "base", "bdo", "blockquote", "body", "br", "button", "canvas",
-    "caption", "cite", "code", "col", "colgroup", "command", "datalist",
-    "dd", "del", "details", "dfn", "div", "dl", "dt", "em", "embed",
-    "eventsource", "fieldset", "figcaption", "figure", "footer", "form", "h1",
-    "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html",
-    "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend",
-    "li", "link", "mark", "html-map", "menu", "html-meta", "meta", "meter",
-    "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p",
-    "param", "pre", "progress", "q", "ruby", "rp", "rt", "samp", "script",
-    "section", "select", "small", "source", "span", "strong", "style", "sub",
-    "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th",
-    "thead", "html-time", "title", "tr", "ul", "video", "wbr"
-],
-$ = {},
-tag;
+var $ = {};
 
 (function () {
+    var tags = [
+        "a", "abbr", "address", "area", "article", "aside", "audio",
+        "b", "base", "bdo", "blockquote", "body", "br", "button", "canvas",
+        "caption", "cite", "code", "col", "colgroup", "command", "datalist",
+        "dd", "del", "details", "dfn", "div", "dl", "dt", "em", "embed",
+        "eventsource", "fieldset", "figcaption", "figure", "footer", "form", "h1",
+        "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html",
+        "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend",
+        "li", "link", "mark", "html-map", "menu", "html-meta", "meta", "meter",
+        "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p",
+        "param", "pre", "progress", "q", "ruby", "rp", "rt", "samp", "script",
+        "section", "select", "small", "source", "span", "strong", "style", "sub",
+        "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th",
+        "thead", "html-time", "title", "tr", "ul", "video", "wbr"
+    ];
+
     function parseAttributes(options) {
         var attributes = [], option;
         for (option in options) {
             if (options.hasOwnProperty(option)) {
-                attributes.push(option + 
+                attributes.push(option +
                     (options[option] !== undefined ?
-                        "=\"" + options[option] + "\"" : ""));
+                            "=\"" + options[option] + "\"" : ""));
             }
         }
         return attributes;
     }
 
     function buildTag(el) {
-        return function (/*options, content, ...*/) {
+        return function () {
             var options = (typeof arguments[0] === "object" ? arguments[0] : undefined),
                 attributes = (options ? parseAttributes(options) : []),
-                inc = (options ? 1 : 0), lim,
+                inc = (options ? 1 : 0),
+                lim,
                 contents = [];
 
             for (lim = arguments.length; inc < lim; inc += 1) {
@@ -42,8 +43,8 @@ tag;
             return "<" + el +
                 (attributes.length > 0 ? " " + attributes.join(" ") : "") +
                 (contents.length >  0 ?
-                    ">" + contents.join("") + "</" + el :
-                    "/") +
+                        ">" + contents.join("") + "</" + el :
+                        "/") +
                 ">";
         };
     }
@@ -64,73 +65,54 @@ tag;
     $["$.linkTo"] = buildMacro($["$." + a], ["href"]);
     */
 
-    $["!DOCTYPE"] = function (/*options, html*/) {
-        var options = (typeof arguments[0] === "object" ?
-                arguments[0] : undefined),
-            attributes,
+    $["!DOCTYPE"] = function (options) {
+        var options = (typeof arguments[0] === "object" ? arguments[0] : undefined),
+            attributes = options ? parseAttributes(options) : [],
             html = arguments[(options ? 1 : 0)] || "";
 
-        if (options) {
-            attributes = parseAttributes(arguments[0]);
-        }
         return "<!DOCTYPE" +
             (attributes.length > 0 ? " " + attributes.join(" ") : "") +
             ">" + html;
-    }
+    };
+
+    var tag;
 
     for (tag in tags) {
-        $[tags[tag]] = buildTag(tags[tag]);
+        if (tags.hasOwnProperty(tag)) {
+            $[tags[tag]] = buildTag(tags[tag]);
+        }
     }
 
     $.image = function (src) {
         return $.img({src: src});
     };
 
-    $.linkTo = function (href/*, content, ...*/) {
+    $.linkTo = function (href) {
         return $.a.apply(this,
             [{href: href}].concat(Array.prototype.slice.call(arguments, 1)));
     };
 
     $.list = function (options, items) {
         return $.ul(options, (function () {
-            var inc = 0, lim = items.length,
-                content = [];
-            return items.map(function (el) {
-                return $.li.apply(this, items[inc]);
+            return items.map(function (item) {
+                return $.li.apply(this, item);
             }).join("");
         }()));
     };
 
     $.javascript = function (options, src) {
-        return $.script(({type: "text/Javascript", src: src}).merge(options), "");
-    }
-
-    $.header = function (title) {
-        return $.div({id: "header", "class": "header"},
-            $.h1({style: "float:left"},
-                title),
-            $.list({id: "navbar", "class": "horizontal_list"}, [
-                    [{id: "menu1"}, $.linkTo("/home", $.image("./images/item.png"), " Home")],
-                    [{id: "menu2"}, $.linkTo("/pricing", $.image("./images/item.png"), " Pricing")],
-                    [{id: "menu3"}, $.linkTo("/about", $.image("./images/item.png"), " About")],
-                    [{id: "menu4"}, $.linkTo("/contact", $.image("./images/item.png"), " Contact")]
-                ])
-        );
+        return $.script(({"type": "text/Javascript", "src": src}).merge(options), "");
     };
 
-    $.standardHead = function (title/*, content, ...*/) {
-        return $["!DOCTYPE"]({html:undefined},
+    $.standardPage = function (title, head) {
+        return $["!DOCTYPE"]({"html": undefined},
             $.html.apply(this, [
-                    {lang: "en", "class": "js"},
-                    $.head(
-                        $.meta({"http-equiv": "X-UA-Compatible", content: "IE=8;chrome=1"}),
-                        $.meta({charset: "utf-8"}),
-                        $.title(title),
-                        $.link({type: "text/css", rel: "stylesheet", href: "/style.css"})/*,
-                        $.link({rel: "shortcut icon", href: "favicon.png"}),
-                        $.link({type: "text/css", rel: "stylesheet", media:"print", href: "css/print.css"})*/),
-                ].concat(Array.prototype.slice.call(arguments, 1))));
+                {"lang": "en", "class": "js"},
+                $.head(
+                    $.meta({"http-equiv": "X-UA-Compatible", "content": "IE=8;chrome=1"}),
+                    $.meta({"charset": "utf-8"}),
+                    $.title(title) + (head || "")
+                )
+            ].concat(Array.prototype.slice.call(arguments, 2))));
     };
 }());
-
-exports.$ = $;
